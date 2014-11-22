da9663c297ae1610f3aaf8372972367699921d88
SkyMomentum <rsdrew@gmail.com>
2014-11-22 01:43:19 -0500

    Start making some waves.

--- pirate.js
+++ pirate.js
@@ -3,8 +3,11 @@ var ATavern = ATavern || {};
 function Ship(id, name) {
     this.id = id;
     this.name = name;
+    this.heading = 0;
+    this.speed = 0;
     this.cargoworth = 0;
     this.loot = [];
+    this.captain = {}
 }
 
 function Seafarer(id, name, shipid) {
@@ -32,6 +35,23 @@ function PirateCaptain(id, name, shipid) {
 }
 PirateCaptain.prototype = Object.create(Pirate.prototype);
 
+
+ATavern.activeCargoShips = [];
+
+ATavern.nextCaptId = 1
+ATavern.makeNewMerchantCaptain = function(name, loot){
+    var tCapt = new Seafarer(2, "Capt. Dull", id);
+}
+
+ATavern.makeCargoship = function(captain, worth) {
+    var tShip = new Ship(id, "Freighter One");
+    
+    tShip.captain = tCapt;
+    tShip.worth = worth;
+    tShip.loot.push(loot);
+    return tShip;
+}
+
 ATavern.appendGameLog = function(str){
     var nline = $("<div></div>", {
       "text": str,
@@ -44,5 +64,7 @@ ATavern.appendGameLog = function(str){
 
 ATavern.initializeGame = function (){
     $( ".GameLog" ).append("WELCOME TO YOUR NEW TAVERN'S DEBUG LOG - ARRRR\n");
-    ATavern.appendGameLog("TEST TEST TEST TEST");
+    ATavern.appendGameLog("Creating 100 cargo ships");
+    var cargoship = ATavern.makeCargoship(1,1);
+    ATavern.appendGameLog("Creating 20 pirate ships");
 }
